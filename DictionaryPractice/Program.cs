5acb4f7a67e7f012b52931f366a865bb07383c70
ABardot <aabardot5@gmail.com>
2018-02-05 09:57:02 -0500

    First commit

---
+++ DictionaryPractice/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace DictionaryPractice
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Syntax Dictionary <Key data type, value data type> dictionary
+            Dictionary<string, int> studentGrades = new Dictionary<string, int>()
+            {
+                { "Kerry", 90 },
+                { "Barry", 85 },
+                { "Michelle", 100 }
+            };
+        }
+    }
+}
