f5157830c04bd73599b11e4e54679cfbeed807f7
ABardot <aabardot5@gmail.com>
2018-02-05 11:10:53 -0500

    Entered more dictionary examples

--- DictionaryPractice/Program.cs
+++ DictionaryPractice/Program.cs
@@ -25,7 +25,7 @@ namespace DictionaryPractice
 
             Dictionary<int, string> drinkMachine = new Dictionary<int, string>(); // Set up with no values, this is set up to take values
 
-            // Using the Add() method, lets add drinks to our drink machine. 
+            // Using the Add() method, lets add drinks to our drink machine.
             // This machine uses just a number to vend as item instead of a letter number
             drinkMachine.Add(10, "Ginger Ale");
             drinkMachine.Add(11, "Bottled Water");
@@ -34,9 +34,90 @@ namespace DictionaryPractice
             drinkMachine.Add(14, "Sprite");
             drinkMachine.Add(15, "Sparkling Water");
 
+            Console.WriteLine(drinkMachine.Count); // Count is a property that returns the size of the dictionary
 
+            // We have another property, called, Key
+            foreach (KeyValuePair<int, string> drink in drinkMachine)
+            {
+                Console.WriteLine(drink.Key);
+            }
 
-            Console.WriteLine(drinkMachine.Count); // Count is a property that returns the size of the dictionary 
+            foreach (KeyValuePair<int, string> drink in drinkMachine)
+            {
+                Console.WriteLine(drink.Value);
+            }
+
+            //// Create a dictionary for a theater coat check, the key will be a number and the value the coat style. Print all values
+            //Dictionary<int, string> coatCheck = new Dictionary<int, string>()
+            //{
+            //    {1, "Winter jacket" },
+            //    {2, "Summer jacket" },
+            //    {3, "Autmun Jacket" },
+            //    {4, "Spring Jacket"},
+            //    {5, "Ski Jacket" },
+            //    {6, "Ski Jacket" },
+            //    {7, "Hunting jacket" },
+            //    {8, "Motorcycle jacket" },
+            //    {9, "Camo Jacket" },
+            //    {10, "black Jacket" }
+            //};
+
+            //foreach (KeyValuePair<int, string> coat in coatCheck)
+            //{
+            //    Console.WriteLine(coat.Key);
+            //}
+            //Create a dictionary for a car valet service with ten cars, the key will be the customers last name and the value
+            //will be the car make. Print to console
+            //Dictionary<string, string> carValet = new Dictionary<string, string>()
+            //{
+            //    {"Smith" , "BMW" },
+            //    {"Sam" , "Ford" },
+            //    {"Bond", "Austin Martin" },
+            //    {"Sanders", "Toyota"},
+            //    {"Thomas", "Mercedes Benz" },
+            //    {"Trump", "Limo" },
+            //    {"Bardot", "Audi" },
+            //    {"Clinton", "Nissan" },
+            //    {"Clinton", "Nissan" },
+            //    {"Brady", "Ferrari" }
+            //};
+            //foreach (KeyValuePair<string, string> car in carValet)
+            //{
+            //    Console.WriteLine(car.Key);
+            //}
+
+            Dictionary<string, int> zooAnimal = new Dictionary<string, int>()
+            {
+                {"Pig",10  },
+                {"Zebra",22 },
+                {"Horse",36 },
+                {"Elephant", 42},
+                {"Shark",65},
+                {"Bear", 68},
+                {"Snake", 77},
+                {"Beatles", 89},
+                {"Monkey", 99},
+                { "Giraffe", 10}
+            };
+
+            int highest = 0;
+            foreach (KeyValuePair<string, int> animal in zooAnimal)
+            {
+                if (animal.Value > highest)
+                {
+                    highest = animal.Value;
+                }
+            }
+
+            Console.WriteLine(highest);
+
+            foreach (KeyValuePair<string, int> animal in zooAnimal)
+            {
+                if (animal.Value == highest)
+                {
+                    Console.WriteLine(animal.Key);
+                }
+            }
         }
     }
 }
